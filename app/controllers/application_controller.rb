b95f22d217aa83f7ac9ca664df514f9b674fc098
mmi-ku <161840539+mmi-ku@users.noreply.github.com>
2024-10-16 13:57:50 +0000

    ストロングパラメーターdevise user分変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except: [:top]
-  def after_sign_in_path_for(resource)
-    about_path
-  end
-  
-  def after_sign_out_path_for(resource)
-    about_path
-  end
-  
-  protected
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+
+
+  # protected
+
 end
